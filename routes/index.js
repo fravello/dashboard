47cc904a5156623c711412892d587af5f00f9754
fravello <fabian.ravello@gmail.com>
2019-12-08 17:55:46 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,19 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/../public/login.html'))
 });
 
+router.post('/login', function(req, res, next) {
+
+  if(req.body.txtUsuario == "admin" && req.body.txtPassword == "taitao123"){
+    res.redirect('/dashboard');
+  }else{
+    res.redirect('/');
+  }
+  
+});
+
+router.get('/dashboard', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname + '/../public/dashboard.html'))
+});
+
 module.exports = router;
