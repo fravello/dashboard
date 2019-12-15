f23800b94d2a9bca3740c55de76c922847ca888d
fravello <fabian.ravello@gmail.com>
2019-12-15 00:30:32 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+var mqtt_dash = require('../models/mqtt_dash');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
@@ -21,6 +23,25 @@ router.post('/login', function(req, res, next) {
 router.get('/dashboard', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.sendFile(path.join(__dirname + '/../public/dashboard.html'))
+});
+
+router.post('/getTemperatura', async function(req, res, next) {
+
+  var fchInicio = req.body.fchInicio + ' 00:00:00';
+  var fchTermino = req.body.fchTermino + ' 23:59:59';
+
+  var result = await mqtt_dash.find({
+                          payload:{$regex: 'tmp'},
+                          date: {
+                            $gte: fchInicio,
+                            $lt: fchTermino
+                          }}).sort({$natural:-1}); //.limit(20)
+
+  console.log(result);
+
+
+  res.send(result);
+  
 });
 
 module.exports = router;
